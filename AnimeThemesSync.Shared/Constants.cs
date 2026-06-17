ab6e144203d78a712d5abb74a26efa08cd378580
yuuta0331 <miyagi.hiroshi@taupe.plala.or.jp>
2026-06-17 22:05:42 +0900

    Add configuration presets

--- AnimeThemesSync.Shared/Constants.cs
+++ AnimeThemesSync.Shared/Constants.cs
@@ -13,12 +13,12 @@ public static class Constants
     /// <summary>
     /// UI asset version used to force web clients to pick up changed embedded pages.
     /// </summary>
-    public const string UiAssetVersion = "20260617b";
+    public const string UiAssetVersion = "20260617c";
 
     /// <summary>
     /// Human-readable UI version displayed on plugin pages.
     /// </summary>
-    public const string UiDisplayVersion = "2026.06.17-b";
+    public const string UiDisplayVersion = "2026.06.17-c";
 
     /// <summary>
     /// The display name used by the metadata provider (matches AnimeThemesMetadataProvider.Name).
